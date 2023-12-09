1e5b07f8711e0b9760747f7adccec73796ec5a98
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2023-12-09 20:17:50 -0300

    Add 3 singletonPattern examples

---
+++ singletonPattern/singletonPatternDoubleChecked/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        MyClass myClass = MyClass.getInstance();
+
+        myClass.name = "Class";
+
+        System.out.println(myClass);
+
+
+
+    }
+}
---
+++ singletonPattern/singletonPatternSimple/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        MyClass myClass = MyClass.getInstance();
+        myClass.name = "Class";
+    }
+}
