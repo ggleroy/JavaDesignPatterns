7044ec8ae4f27a83dfc2c8c997d19b6dcd241829
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2023-12-16 21:17:23 -0300

    Add singleton2

---
+++ singleton/singleton2/src/MyClass.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    private static MyClass uniqueInstance;
+    String name;
+    private MyClass(){
+    }
+    public static MyClass getInstance(){
+        if(uniqueInstance == null) {
+            uniqueInstance = new MyClass();
+        }
+
+        return uniqueInstance;
+    }
+}
